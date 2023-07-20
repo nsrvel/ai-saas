f01f4500be018e5ab2677d416ab9d76f0101c51b
Putra <putra.rama@suretybond.co.id>
2023-07-20 18:00:56 +0700

    initial commit

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ const DashboarLayout = ({ children }: { children: React.ReactNode }) => {
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
                 <Sidebar />
             </div>
-            <main className="md:pl-72">
+            <main className="md:pl-72 pb-10">
                 <Navbar />
                 {children}
             </main>
--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { Menu } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
@@ -7,6 +8,16 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components//ui/sheet';
 import Sidebar from '@/components/sidebar';
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
     return (
         <Sheet>
             <SheetTrigger>
